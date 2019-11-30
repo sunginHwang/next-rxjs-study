6e070c47b6317682bfaae480a01ac061f45ecd56
hwangseong-in <gommpo111@gmail.com>
2019-11-30 15:50:56 +0900

    test for cold observeble

--- pages/operators.tsx
+++ pages/operators.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import {Observable, pipe, range} from 'rxjs';
-import {filter, find, isEmpty, map, count} from "rxjs/operators";
+import {filter, find, isEmpty, map, count, skip, startWith, endWith} from "rxjs/operators";
 
 const Operators = ({}) => {
 
     const countFive = range(1, 10)
         .pipe(
+            skip(5),
             count(v => !(v % 2)),
         );
     console.log('count');
     console.log(countFive.subscribe(v => console.log(v)));
 
+    const withTest = range(1, 10)
+        .pipe(
+            startWith(998),
+            endWith(2000),
+        );
+    console.log('withTest');
+    withTest.subscribe(v => console.log(v));
+
     const a = range(1, 10)
         .pipe(
+            startWith(998),
+            endWith(2000),
             filter(v => !(v % 2)),
             map(v => v + v),
         );
@@ -21,6 +32,8 @@ const Operators = ({}) => {
     a.subscribe(v => console.log(v));
     console.log('secondCall');
     a.subscribe(v => console.log(v));
+    setTimeout(()=> a.subscribe(v => console.log(v)),800);
+    console.log('test for async case');
 
     const getRangeObservable = count => range(1, count);
 
@@ -40,6 +53,7 @@ const Operators = ({}) => {
     subscribeWithIsEmpty(3);
     subscribeWithFind(1);
     subscribeWithFind(4);
+
 
     return (
         <div>
